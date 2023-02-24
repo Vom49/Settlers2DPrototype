3077ae4da482a48388d182d79132177a851b20a9
Vom49 <121884139+Vom49@users.noreply.github.com>
2023-02-24 16:49:24 +0000

    Vertex Drawing And Placement

--- Assets/Scripts/GridControlScript.cs
+++ Assets/Scripts/GridControlScript.cs
@@ -6,10 +6,16 @@ public class GridControlScript : MonoBehaviour
 {
     //prefab for the hex that it will draw
     [SerializeField] private GameObject _HexPrefab;
+    [SerializeField] private GameObject _VertexPrefab;
     [HideInInspector] public GameObject[,] HexGridArray;
 
+    float xOffset = 0.457f;
+    float yOffset = 0.795f;
 
-    
+    //width and height in hexes, nothing to do with unity coords
+    int gridWidth = 7;
+    int gridHeight = 7;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,14 +29,10 @@ public class GridControlScript : MonoBehaviour
         
     }
 
+    //draws the grid using an Axial method of storing the grid
+    //Axial hex grid means that a column cascades down to the right
     private void drawGridAxial()
     {
-        //width and height in hexes, nothing to do with unity coords
-        int gridWidth = 5;
-        int gridHeight = 5;
-
-        float xOffset = 0.457f;
-        float yOffset = 0.795f;
         HexGridArray = new GameObject[gridWidth, gridHeight];
         for (int i = 0; i < gridWidth; i++)
         {
@@ -44,21 +46,32 @@ public class GridControlScript : MonoBehaviour
                 hexInstance.transform.SetParent(this.gameObject.transform);
                 hexInstance.name = "Hex_" + i + "_" + j;
                 HexGridArray[i, j] = hexInstance;
+                drawVertices(hexInstance, i, j);
             }
         }
-        //removes hexes that are not on the catan board
 
-        Destroy(GameObject.Find("Hex_0_0"));
-        HexGridArray[0, 0] = null;
-        Destroy(GameObject.Find("Hex_0_1"));
-        HexGridArray[0, 1] = null;
-        Destroy(GameObject.Find("Hex_1_0"));
-        HexGridArray[1, 0] = null;
-        Destroy(GameObject.Find("Hex_3_4"));
-        HexGridArray[3, 4] = null;
-        Destroy(GameObject.Find("Hex_4_3"));
-        HexGridArray[4, 3] = null;
-        Destroy(GameObject.Find("Hex_4_4"));
-        HexGridArray[4, 4] = null;
+        
+        //removes hexes that are not on the catan board
+        Destroy(GameObject.Find("Hex_1_1"));
+        HexGridArray[1, 1] = null;
+        Destroy(GameObject.Find("Hex_1_2"));
+        HexGridArray[1, 2] = null;
+        Destroy(GameObject.Find("Hex_2_1"));
+        HexGridArray[2, 1] = null;
+        Destroy(GameObject.Find("Hex_5_4"));
+        HexGridArray[5, 4] = null;
+        Destroy(GameObject.Find("Hex_5_5"));
+        HexGridArray[5, 5] = null;
+        Destroy(GameObject.Find("Hex_4_5"));
+        HexGridArray[4, 5] = null;
+    }
+
+    //draws the vertices of the hex that is inputted, used inside the draw grid loop
+    private void drawVertices(GameObject targetHex, int hexX, int hexY)
+    {
+        GameObject vertexInstance1 = (GameObject)Instantiate(_VertexPrefab, new Vector2(targetHex.transform.position.x, targetHex.transform.position.y + 0.5f), Quaternion.identity);
+        vertexInstance1.name = "Vertex_" + hexX + "_" + hexY + "_1"; //north vertex
+        GameObject vertexInstance2 = (GameObject)Instantiate(_VertexPrefab, new Vector2(targetHex.transform.position.x + 0.455f, targetHex.transform.position.y + 0.28f), Quaternion.identity);
+        vertexInstance1.name = "Vertex_" + hexX + "_" + hexY + "_2"; //north east vertex
     }
 }
